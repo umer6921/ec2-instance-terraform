b4eb98d006e63db4fb609fbacce4ec0ff71d3fdc
Muhammad Umer <umerm6921@gmail.com>
2023-10-08 15:28:39 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,13 +8,3 @@ resource "aws_s3_bucket" "demo3_s3" {
 
 
 }
-
-resource "aws_instance" "demo-ec2" {
-  ami = var.ec2-ubuntu-ami
-  tags = {
-    name = "terra-auto-instance"
-  }
-  instance_type = "t2.micro"
-  
- 
-}
